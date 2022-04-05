4aea7c9aee91a9bfb9ef50bf152ef76704dc608f
Amplify Rebel <erj2204@gmail.com>
2022-04-04 21:09:51 -0500

    added the forcast and local saves with uv

---
+++ assets/script/script.js
@@ -0,0 +1,78 @@
+var cityList = [];
+var id = "5859ec0dbfd9ff0a36abca355158892e";
+function storeCities() {
+    localStorage.setItem("cities", JSON.stringify(cityList));
+
+}
+
+function createCityList(){
+    $(".cityList").empty();
+    cityList.forEach(function(city) {
+        $(".cityList").prepend($(`<button class="list-group-item list-group-item-action cityButton" data-city="${city}">${city}</button>`));
+    })
+}
+
+function init() {
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+
+    if (storedCities !== null) {
+        cityList = storedCities;
+    }
+
+    createCityList();
+
+    if (cityList) {
+        var thisCity = cityList[cityList.length - 1]
+        getCurrentWeather(thisCity, id);
+        getForecast(thisCity, id);
+    }
+}
+function getCurrentWeather(thisCity, id) {
+    var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${thisCity}&units=imperial&appid=${id}`;
+    var cityLat;
+    var cityLong;
+
+    $.ajax({
+        url: weatherURL,
+        method: "GET"
+    }).then(function (data) {
+        $(".cityToday").append(
+            `<div class="row ml-1">
+                <h3 class="mr-3">${data.name} (${(new Date(1000 * data.dt).getUTCMonth()) + 1}/${(new Date(1000 * data.dt).getUTCDate()) - 1}/${new Date(1000 * data.dt).getUTCFullYear()})</h3>
+                <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">
+            </div>`
+        )
+        $(".cityToday").append(`<p>Temperature: ${data.main.temp} &degF</p>`)
+        $(".cityToday").append(`<p>Humidity: ${data.main.humidity} %</p>`)
+        $(".cityToday").append(`<p>Wind: ${data.wind.speed} mph</p>`)
+        cityLat = data.coord.lat;
+        cityLong = data.coord.lon;
+        getUVI(id, cityLat, cityLong);
+    })
+
+}
+function getUVI(id, cityLat, cityLong) {
+    var uvURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${cityLat}&lon=${cityLong}&appid=${id}`;
+
+    $.ajax({
+        url: uvURL,
+        method: "GET"
+    }).then(function (data) {
+        $(".cityToday").append(`<p>UV Index: <span class="badge badge-danger p-2">${data.value}</span></p>`);
+    })
+}
+
+// main function that clears divs and calls current and 5-day forecasts for city
+function displayCityWeather() {
+    var thisCity = $(this).attr("data-city");
+
+    $(".cityToday").empty();
+    getCurrentWeather(thisCity, id);
+
+    $(".forecast").empty();
+    getForecast(thisCity, id);
+    
+}
+
+// calls main on page load function
+init();
